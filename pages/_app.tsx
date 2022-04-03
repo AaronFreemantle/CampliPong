f5b9ff05154a2a8913ba88328c672ec49ebcc520
AaronFreemantle <afreemantle@camplify.com.au>
2022-04-03 20:42:08 +1000

    Add head title and description

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,9 +1,14 @@
 import { SessionProvider } from 'next-auth/react'
 import { AppProps } from "next/app";
+import Head from 'next/head';
 
 const App = ({ Component, pageProps }: AppProps) => {
   return (
     <SessionProvider session={pageProps.session}>
+      <Head>
+        <title>Camplipong</title>
+        <meta name='description' content='Camplify Ping Pong Leaderboard'/>
+      </Head>
       <Component {...pageProps} />
     </SessionProvider>
   );
