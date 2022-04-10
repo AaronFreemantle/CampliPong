115ee2bc48fdc1f73edb074132998bfecfd58845
AaronFreemantle <afreemantle@camplify.com.au>
2022-04-10 16:43:21 +1000

    Basic Leaderboard

--- pages/api/matches/index.tsx
+++ pages/api/matches/index.tsx
@@ -1,7 +1,10 @@
 import { NextApiRequest, NextApiResponse } from "next";
 import prisma from "../../../lib/prisma";
 
-export default function handler(req: NextApiRequest, res: NextApiResponse) {
-  const matches = prisma.match.findMany();
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
+  const matches = await prisma.match.findMany();
   res.status(200).json(matches);
 }
--- pages/leaderboard.tsx
+++ pages/leaderboard.tsx
@@ -1,94 +1,54 @@
+import { Prisma } from "@prisma/client";
 import React from "react";
-import { useTable, Column } from "react-table";
+import prisma from "../lib/prisma";
 
-type Data = {
-  id: string;
-  date: string;
-  firstName: string;
-  lastName: string;
-  email: string;
-  view: React.ReactNode;
-};
+type UsersWithScores = Prisma.UserGetPayload<{
+  include: { userScores: true };
+}>;
 
-const createArr = (n: number): Data[] => {
-  const data: Data[] = [];
-  for (let i = 0; i < n; i += 1) {
-    data.push({
-      id: `ID-${Math.random().toFixed(4)}`,
-      date: new Date().toDateString(),
-      firstName: `Rick #${i}`,
-      lastName: `Sanchez #${i}`,
-      email: `morty_${i}@rick.space`,
-      view: <button>View</button>,
-    });
-  }
-  return data;
-};
-
-export function Leaderboard() {
-  const data = React.useMemo<Data[]>(() => createArr(10), []);
-  const columns = React.useMemo<Column<Data>[]>(
-    () => [
-      {
-        Header: "ID",
-        accessor: "id",
-      },
-      {
-        Header: "Date Requested",
-        accessor: "date",
-      },
-      {
-        Header: "First Name",
-        accessor: "firstName",
-      },
-      {
-        Header: "Last Name",
-        accessor: "lastName",
-      },
-      {
-        Header: "Email",
-        accessor: "email",
-      },
-      {
-        accessor: "view",
-      },
-    ],
-    []
-  );
-  const { getTableProps, getTableBodyProps, headerGroups, rows, prepareRow } =
-    useTable<Data>({ columns, data });
-
+function Table({ users }: { users: UsersWithScores[] }) {
   return (
-    <table {...getTableProps()} style={{ border: "solid 1px blue" }}>
+    <table>
       <thead>
-        {headerGroups.map((headerGroup) => (
-          <tr key={headerGroup.id} {...headerGroup.getHeaderGroupProps()}>
-            {headerGroup.headers.map((column) => (
-              <th key={column.id} {...column.getHeaderProps()}>
-                {column.render("Header")}
-              </th>
-            ))}
+        <tr>
+          <th>Name</th>
+          <th>Rating</th>
+          <th>Wins</th>
+          <th>Losses</th>
+          <th>Played</th>
+          <th>Win Rate</th>
+        </tr>
+      </thead>
+      <tbody>
+        {users.map((user) => (
+          <tr key={user.id}>
+            <td>{user.name}</td>
+            <td>{user.rating}</td>
+            <td>{user.userScores.filter((score) => score.userWon).length}</td>
+            <td>{user.userScores.filter((score) => !score.userWon).length}</td>
+            <td>{user.userScores.length}</td>
+            <td>
+              {(user.userScores.filter((score) => score.userWon).length /
+                user.userScores.length) *
+                100}
+              %
+            </td>
           </tr>
         ))}
-      </thead>
-      <tbody {...getTableBodyProps()}>
-        {rows.map((row) => {
-          prepareRow(row);
-          return (
-            <tr key={row.id} {...row.getRowProps()}>
-              {row.cells.map((cell) => {
-                return (
-                  <td key={cell.value} {...cell.getCellProps()}>
-                    {cell.render("Cell")}
-                  </td>
-                );
-              })}
-            </tr>
-          );
-        })}
       </tbody>
     </table>
   );
 }
 
-export default Leaderboard;
+export default Table;
+
+export async function getServerSideProps() {
+  const users = await prisma.user.findMany({
+    include: { userScores: true },
+  });
+  return {
+    props: {
+      users: JSON.parse(JSON.stringify(users)),
+    },
+  };
+}
